f81aafff0551f2edc2dfebe64e229b0812e8fcc8
aybekckaya <aybekcankaya@icloud.com>
2015-04-30 12:06:21 +0300

    Initial Commit

---
+++ PlistOperations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlistOperations
+//
+//  Created by aybek can kaya on 4/30/15.
+//  Copyright (c) 2015 aybek can kaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlistOperations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlistOperations
+//
+//  Created by aybek can kaya on 4/30/15.
+//  Copyright (c) 2015 aybek can kaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
